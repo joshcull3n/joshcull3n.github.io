6946b995e217b91cf049fccf8870a5ed16350003
joshcull3n <8888780+joshcull3n@users.noreply.github.com>
2024-02-06 11:04:59 -0800

    front end rework

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import resume from '../files/Sept_2023.pdf'
 import todo_gif from '../images/todo.gif'
 import todo_webm from '../images/todo.webm'
 import habits_pic from '../images/habits.png'
+import './light.css'
+import './dark.css'
 
 const TopBar = () => {
   return (
-    <div className="top-block" style={{paddingRight:'0px'}}>
-      <div className="is-layout-flex centered" style={{ display: 'flex', padding: '2.25rem', paddingBottom: '0rem', marginLeft: '40px', marginRight: '55px'}}>
-        <a href="index.html">
-          <img decoding="async" src={icon} alt="me" width="100" height="100" />
-        </a>
-        <div style={{width:'140px', minWidth: '133.5px', paddingLeft: '10px', paddingRight: '20px', textAlign: 'center'}}>
-          <h2 style={{fontSize: '22px'}}>josh cullen</h2>
-        </div>
-        <div>
-        <p style={{ textAlign: 'right', textUnderlineOffset: '2px', paddingRight: '0px' }}>
-          <a href="https://github.com/joshcull3n">github</a><br/>
-          <a href="https://soundcloud.com/mrmoooon">soundcloud</a><br/>
-          <a href="https://www.linkedin.com/in/jlcullen/">linkedin</a>
-        </p>
+    <div>
+      <div className="is-layout-flex centered">
+        <div className="is-layout-flex centered topBar" style={{ margin: '0.5rem 0px 0px 0px' }}>
+          <a href="index.html">
+            <img decoding="async" src={icon} alt="me" width="80" height="80" style={{ marginTop: '0.4rem', borderRadius: '3px' }} />
+          </a>
+          <div style={{width:'140px', minWidth: '133.5px', textAlign: 'center'}}>
+            <h2 style={{fontSize: '22px'}}>josh cullen</h2>
+          </div>
+          <div>
+          <p style={{ margin: 0, textAlign: 'right', textUnderlineOffset: '2px', paddingRight: '0px' }}>
+            <a href="https://github.com/joshcull3n">github</a><br/>
+            <a href="https://soundcloud.com/mrmoooon">soundcloud</a><br/>
+            <a href="https://www.linkedin.com/in/jlcullen/">linkedin</a><br/>
+            <a href={resume}>resume</a>
+          </p>
+          </div>
         </div>
       </div>
       <Intro />
@@ -30,13 +35,13 @@ const TopBar = () => {
 
 const Intro = () => {
   return (
-    <div className="is-layout-flex centered" style={{marginTop:'0px', marginLeft:'5px'}}>
-    <div style={{width: '405px',justifyContent: 'left', alignItems:'left'}}>
-      <p style={{textAlign:'left', marginTop: '0px'}}>los angeles based web & automation dev.
-        <br/>currently head of QA at <a href="https://www.gridspace.com/">Gridspace</a>.
-        </p>
-    </div>
-    </div>
+      <div className="is-layout-flex centered" style={{ }}>
+        <div className="introBar" style={{ borderRadius: '3px', marginTop: '0.3%' }}>
+          <p style={{marginTop: '0px', marginBottom: '1.5%', textAlign: 'center'}}>los angeles based web & automation dev.
+            <br/>currently head of QA at <a href="https://www.gridspace.com/">Gridspace</a>.
+            </p>
+        </div>
+      </div>
   )
 }
 
@@ -44,7 +49,6 @@ const Footer = () => {
   return (
     <footer className="footer" style={{ padding: '1rem 0 1rem', width: '100%', textAlign: 'center'}}>
       <div>
-        <a style={{backgroundColor:'rgba(0,0,0,0.3)', padding: '0 3px 3px 3px'}} href={resume}>peruse my resume</a>
       </div>
     </footer>
   )
@@ -59,8 +63,8 @@ const HorizontalGallery = (props) => {
           <tbody>
             <tr>
               <td style={{paddingRight:'15px'}}><em><p style={{fontSize:'20px'}}>projects</p></em></td>
-              <td className='galleryTd'><GalleryItem imgUrl={habits_pic} link='/habits' title='habits' /></td>
-              <td className='galleryTd'><GalleryItem imgUrl={todo_gif} link='https://github.com/joshcull3n/todo' title='todo'/></td>
+              <td className='galleryTd'><GalleryItem imgUrl={habits_pic} link='/habits' title='habits' imgId='habitsLink' mobile={true}/></td>
+              <td className='galleryTd'><GalleryItem imgUrl={todo_gif} link='https://github.com/joshcull3n/todo' title='todo' imgId='todoLink' mobile={true}/></td>
             </tr>
           </tbody>
         </table>
@@ -73,8 +77,8 @@ const HorizontalGallery = (props) => {
         <table className='navBar'>
           <tbody>
             <tr>
-              <td className='galleryTd'><GalleryItem imgUrl={habits_pic} link='/habits' title='habits' /></td>
-              <td className='galleryTd'><GalleryVideo video={todo_webm} link='https://github.com/joshcull3n/todo' title='todo'/></td>
+              <td className='galleryTd'><GalleryItem imgUrl={habits_pic} link='/habits' title='habits' imgId='habitsLink' mobile={false}/></td>
+              <td className='galleryTd'><GalleryVideo video={todo_webm} link='https://github.com/joshcull3n/todo' title='todo' imgId='todoLink' mobile={false}/></td>
             </tr>
           </tbody>
         </table>
@@ -84,10 +88,14 @@ const HorizontalGallery = (props) => {
 }
 
 const GalleryItem = (props) => {
+  let maxHeight = '500px';
+  if (props.mobile) {
+    maxHeight = '80vw'
+  }
   return (
     <div className='galleryDiv'>
       <a className='galleryItem' href={props.link}>
-        <img alt='gallery item thumbnail' style={{maxHeight:'400px', maxWidth: '90vw'}} src={props.imgUrl}/>
+        <img alt='gallery item thumbnail' style={{ maxHeight: maxHeight, maxWidth: '90vw' }} id={props.imgId} src={props.imgUrl}/>
       </a>
       <div className='centered text-fade'>
         <a href={props.link} className='galleryText'>{props.title}</a>
@@ -100,7 +108,7 @@ const GalleryVideo = (props) => {
   return (
     <div className='galleryDiv'>
       <a className='galleryItem' href={props.link}>
-        <video src={props.video} style={{maxHeight:'400px', maxWidth: '90vw'}} autoPlay={true} loop={true} muted={true} className='navPic' alt='gallery item thumbnail'/>
+        <video src={props.video} style={{}} autoPlay={true} loop={true} muted={true} className='navPic' alt='gallery item thumbnail'/>
       </a>
       <div className='centered text-fade'>
         <a href={props.link} className='galleryText'>{props.title}</a>
@@ -122,6 +130,9 @@ function DetectDevice() {
 
 
 const App = () => {
+  // set body class
+  document.body.classList.add("darkMode");
+
   return (
     <div>
       <TopBar />
